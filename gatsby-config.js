39657019088bb13a6045421ea607b623020e8611
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2021-04-03 23:55:47 +0900

    update posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,24 @@ module.exports = {
         plugins: [`gatsby-remark-images`],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: process.env.SITE_GTM_ID,
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        dataLayerName: 'dataLayer',
+      }
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
